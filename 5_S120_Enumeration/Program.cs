be2d6b0aa2a1b13ae800e2c4f601bb39d287ec1a
lfretz <levi.fretz@hsr.ch>
2020-05-19 14:33:11 +0200

    Übungen auf Seite 120

---
+++ 5_S120_Enumeration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _5_S120_Enumeration
+{
+    class Program
+    {
+            enum Weekday
+        {
+            Monday,
+            Tuesday, 
+            Wednesday, 
+            Thursday,
+            Friday, 
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            //S. 120
+            Weekday myDay1 = Weekday.Thursday;
+            Console.WriteLine(myDay1);
+            int weekday = (int)myDay1;
+            Console.WriteLine(myDay1.ToString());
+
+            //S. 123
+            Console.WriteLine("Bitte englischen Wochentag eingeben:");
+            string day = Console.ReadLine();
+            Weekday myDay = (Weekday)Enum.Parse(typeof(Weekday), day);
+            Console.WriteLine($"Der eingegebene Wochentag im Enum-Typ ist:{myDay}");
+
+        }
+    }
+}
