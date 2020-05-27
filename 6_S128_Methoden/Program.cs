cf23b7de53ec7b52e9140a53bfccbc63084c16e5
Levi Fretz <lfretz@hsr.ch>
2020-05-27 11:40:05 +0200

    Kapitel 6 Methoden erarbeiten. (1/2)

---
+++ 6_S128_Methoden/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _6_S128_Methoden
+{
+    class Program
+    {
+        static void Increment(ref int number)
+        {
+            number++;
+        }
+        static bool Divide(double c, double d, out double result)
+        {
+            result = 0.0;
+            if (d == 0.0)
+            {
+                return false;
+            }
+            else
+            {
+                result = c / d;
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Sommerschlussverkauf S.138
+            int number;
+            if (int.TryParse("2", out number))
+                Console.WriteLine("Parse hat funktioniert.");
+            else
+                Console.WriteLine("Keine gültige Zahl.");
+            Console.WriteLine(number);
+            //Beispiel Ausgangsparameter S.139
+            double x = 12;
+            double d = 3;
+
+            bool canDivide = Divide(x, d, out double result);
+            Console.WriteLine("Ist Division möglich: "+canDivide);
+            Console.WriteLine("Resultat = "+result);
+
+
+
+            //Methodenaufruf mit ref S.138
+            int a = 12;
+            Increment(ref a);
+            Console.WriteLine(a);
+
+        }
+    }
+}
