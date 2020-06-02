320b87d5654fbba1364cf5631e6402acb6bba035
lfretz7 <levi.fretz@outlook.com>
2020-06-02 15:11:36 +0200

    Projekt DateisystemSuche hinzugefügt.

---
+++ 6_S150_DateisystemSuche/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_S150_DateisystemSuche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
