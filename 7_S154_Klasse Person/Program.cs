f4ad9263e16cf19536ae2e45872671421192334e
lfretz7 <levi.fretz@outlook.com>
2020-06-03 15:00:00 +0200

    Projekt: 7_S154_Klasse_ausprogrammierter Accessor hinzugefügt eine Klasse per ausprogrammiertem Accessor erstellt Kommentare verfasst

---
+++ 7_S154_Klasse Person/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _7_S154_Klasse_ausprogrammierter Accessor
+{
+    class Program
+    {
+        class Person //neue Klasse: Person
+        {
+            string firstname;
+            public string Firstname //neue Eigenschaft: Firstname, Syntax: Sichtbarkeit Datentyp Name 
+            {
+                get //um Wert einer Eigenschaft auszulesen
+                {
+                    return this.firstname = "Harald"; //Standartwert für firstname
+                }
+                set // um der Eigenschaft einen Wert zuzuweisen. Hier kann auf Gültigkeit überprüft werden. z.B. ob eine korrekte Postleitzahl angegeben wurde.
+                {
+                    this.firstname = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person();
+
+            Console.WriteLine(myPerson.Firstname);
+        }
+    }
+}
