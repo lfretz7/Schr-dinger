b2e5b0b2ca94d3cd521b3b5ad18b7acca38d4ecb
lfretz7 <levi.fretz@outlook.com>
2020-06-08 15:58:08 +0200

    Instanz-Methode(nichtstatisch) hinzugefügt.

---
+++ 7_S170_Instanz-Methode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _7_S170_Instanz_Methode
+{
+    class Program
+    {
+        class Position
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+        }
+        class WoWCharacter
+        {
+            public int Health { get; private set; }
+            public int Level { get; private set; }
+            public Position Position { get; private set; }
+            public float Strength { get; set; }
+            public float Agility { get; set; }
+            public float Speed { get; set; }
+            public void Attack(WoWCharacter character)
+            {
+             
+            }
+            public void Move(Position newposition)
+            {
+
+            }
+            public void Die()
+            {
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
