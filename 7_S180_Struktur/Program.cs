07b2aa7dfd50a5cc6405c2013fad1fb4e367cd0f
lfretz7 <levi.fretz@outlook.com>
2020-06-09 10:10:27 +0200

    Projekt Struktur erstellt und eigenen Wertetyp mit mehreren Datentypen definiert.

---
+++ 7_S180_Struktur/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_S180_Struktur
+{
+    class Program
+    {
+        struct eigenerDatentyp
+        {
+            public int Eigenschaft1 { get; set; }
+            public bool Eigenschaft2 { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            eigenerDatentyp meineVariable1 = new eigenerDatentyp();
+            meineVariable1.Eigenschaft1 = 100;
+            meineVariable1.Eigenschaft2 = true;
+        }
+    }
+}
