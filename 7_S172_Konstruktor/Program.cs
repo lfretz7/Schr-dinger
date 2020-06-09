db951bfc6a4f22fd7159aebd4ad84f2be3af322f
lfretz7 <levi.fretz@outlook.com>
2020-06-09 09:39:06 +0200

    Konstruktor Projekt erstellt

---
+++ 7_S172_Konstruktor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _7_S172_Konstruktor
+{
+    class Program
+    {
+        class Person
+        {
+            public Person()
+            {
+
+            }
+            public Person(string name): this(name, 0)
+            {
+
+            }
+            public Person(string name, float weight)
+            {
+                this.Name = name;
+                this.Weight = weight;
+            }
+            public string Name { get; set; }
+            public float Weight { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Schrödinger");
+            Person p2 = new Person("er", 99);
+            Person p3 = new Person("Noch wer", 70);
+            Console.WriteLine(p2.Name + " " + p2.Weight);
+        }
+    }
+}
