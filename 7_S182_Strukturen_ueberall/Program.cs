287e45ddd14b84fc5845e36c7797e63fb9cf3104
lfretz7 <levi.fretz@outlook.com>
2020-06-09 13:28:47 +0200

    Projekt Strukturen_ueberall erstellt und eine Struktur MinMax mit der Funktion MinMax.

---
+++ 7_S182_Strukturen_ueberall/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _7_S182_Strukturen_ueberall
+{
+    class Program
+    {
+        struct MinMax
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+        }
+        static MinMax Range(int[] nums)
+        {
+            MinMax range = new MinMax();
+            range.Min = int.MaxValue;
+            range.Max = int.MinValue;
+            for (int i = object; i < nums.Length; i++)
+            {
+                if (nums[i] < range.Min)
+                    range.Min = nums[];
+                if (nums[i] > range.Max)
+                    range.Max = nums[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
