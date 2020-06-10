c177a5c7486f282ec5f9493671156e6403faf6a9
lfretz7 <levi.fretz@outlook.com>
2020-06-10 15:09:54 +0200

    Ganzes Projekt in einzelne Files aufgeteilt.

---
+++ 7_S156_weitere_Aufgabe_Accessor/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schroedinger.Klassen
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+
+        private DateTime birthday;
+        public DateTime Birthday
+        {
+            get { return this.birthday; }
+            set
+            {
+                if (value <= DateTime.Now)
+                {
+                    this.birthday = value;
+                }
+            }
+        }
+        public Sex Sex { get; set; }
+    }
+}
--- 7_S156_weitere_Aufgabe_Accessor/Program.cs
+++ 7_S156_weitere_Aufgabe_Accessor/Program.cs
@@ -2,35 +2,18 @@
 
 namespace Schroedinger.Klassen
 {
-    class Program
+    public class Program
     {
-        enum Sex { Male, Female}
-        class Person //neue Klasse: Person
-        {
-            public string Firstname { get; set; }
 
-            DateTime birthday;
-            public DateTime Birthday
-            {
-                get { return this.birthday;}
-                set
-                {
-                    if (value <= DateTime.Now)
-                    {
-                        this.birthday = value;
-                    }
-                }
-            }
-            public Sex Sex { get; set; }
-            static void Main(string[] args)
-            {
-                Person lfretz = new Person(); // Objekt lfretz erstellt
-                lfretz.birthday = DateTime.Parse("12.03.2001 12:00:00");
-                lfretz.Sex = Sex.Male;
-                Console.WriteLine("Geburtsdatum:  " + lfretz.birthday);
-                Console.WriteLine("\n \n \n");
-                Console.WriteLine("Geschlecht:  " + lfretz.Sex);
-            }
+        static void Main(string[] args)
+        {
+            Person lfretz = new Person(); // Objekt lfretz erstellt
+            lfretz.Birthday = DateTime.Parse("12.03.2001 12:00:00");
+            lfretz.Sex = Sex.Male;
+            Console.WriteLine("Geburtsdatum:  " + lfretz.Birthday);
+            Console.WriteLine("\n \n \n");
+            Console.WriteLine("Geschlecht:  " + lfretz.Sex);
         }
+
     }
 }
