12d216570200be42c8b0ee80767ffb7173bee7cf
lfretz7 <levi.fretz@outlook.com>
2020-06-15 14:03:51 +0200

    2 Geisterklassen erstellt. Die erste Geisterklasse vererbt ihre Eigenschaften an die Schleimgeisterklasse. Programm und Klassen wurden in verschiedenen Dateien gespeichert.

---
+++ 9_S208_Erben_ohne_Sterben/Ghosts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9_S208_Erben_ohne_Sterben
+{
+    class Ghost
+    {
+        public Ghost(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+        public virtual void Haunt()
+        {
+            Console.WriteLine("{0} sagt: 'Buh'", this.Name);
+        }
+    }
+    class SlimeGhost : Ghost
+    {
+        public SlimeGhost(string name)
+            : base(name)
+        {
+        }
+        public override void Haunt()
+        {
+            this.Slime();
+            base.Haunt();
+        }
+        public void Slime()
+        {
+            Console.WriteLine("{0} hinterlässt eine Schleimspur", this.Name);
+        }
+    }
+}
---
+++ 9_S208_Erben_ohne_Sterben/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _9_S208_Erben_ohne_Sterben
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ghost ghost1 = new Ghost("Spooky");
+            ghost1.Haunt();
+
+            SlimeGhost ghost2 = new SlimeGhost("Slimey");
+            Console.WriteLine("Slime Ghost Name is: {0}", ghost2.Name);
+            ghost2.Haunt();
+        }
+    }
+}
