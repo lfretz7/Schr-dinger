4b7917291569f1dd31a744f806ecc808e299ce07
lfretz7 <levi.fretz@outlook.com>
2020-06-23 10:59:57 +0200

    Ein Array mit Objekten der Interfaces erstellt und auf die Typinformationen zugegriffen in der Ausgabe.

--- 10_S240_Kaffemaschine_im_Einsatz/Program.cs
+++ 10_S240_Kaffemaschine_im_Einsatz/Program.cs
@@ -6,7 +6,17 @@ namespace _10_S240_Kaffemaschine_im_Einsatz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IHotDrink[] hotDrinks = new IHotDrink[]
+            {
+                new Coffee() {Caffeine = 100, Degree = 90},
+                new Tea {Degree = 95}
+            };
+            foreach (IHotDrink drink in hotDrinks)
+            {
+                Console.WriteLine("Temperatur von {0}: {1}",
+                drink.GetType().Name,
+                drink.Degree);
+            }
         }
     }
 }
