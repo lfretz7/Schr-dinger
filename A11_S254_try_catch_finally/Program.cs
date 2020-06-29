9749218e057ba527eca40f509addee71eb953d9d
lfretz7 <levi.fretz@outlook.com>
2020-06-29 14:43:29 +0200

    Übung zu einer Dateisuche mit try/catch/finally

---
+++ A11_S254_try_catch_finally/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A11_S254_try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = System.IO.File.ReadAllText("c:\\myFile.txt");
+                Console.WriteLine("Der Text in der Datei lautet:");
+                Console.WriteLine(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hoppala. Da gibt es ein Problem:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
