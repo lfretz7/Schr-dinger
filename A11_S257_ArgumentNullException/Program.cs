ebd19907ad60fa75dec5c047403110d13ce17f10
lfretz7 <levi.fretz@outlook.com>
2020-06-30 10:10:44 +0200

    Eine Exception mit dem Argument Null implementiert.

---
+++ A11_S257_ArgumentNullException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A11_S257_ArgumentNullException
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Person p = new Person("Schrödinger");
+                // p.ThrowException();
+            }
+            catch (ArgumentNullException ex) when (ex.InnerException != null)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Ups, bin hier!");
+            }
+        }
+    }
+}
+
