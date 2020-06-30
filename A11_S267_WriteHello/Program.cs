657fdc569cf8a51a3cd1cdf58c586d1f8a04ed76
lfretz <levi.fretz@hsr.ch>
2020-06-30 17:00:00 +0200

    Eine Funktion mit Exceptions implementiert.

---
+++ A11_S267_WriteHello/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace A11_S267_WriteHello
+{
+
+	class Program
+	{
+		static void WriteHello(string name)
+		{
+			if (String.IsNullOrEmpty(name))
+				throw new ArgumentNullException("name");
+			Console.WriteLine("Hello {0}", name);
+		}
+		static void WriteHello2(string name)
+		{
+			if (String.IsNullOrEmpty(name))
+				throw new ArgumentNullException(nameof(name));
+			Console.WriteLine("Hello {0}", name);
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your name");
+			string name = Console.ReadLine().Trim();
+			if (!String.IsNullOrEmpty(name))
+				WriteHello(name);
+			else
+				Console.WriteLine("Warum machst du nie, was man dir sagt?");
+		}
+	}
+}
