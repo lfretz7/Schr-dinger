a730f93a2e82edaef71af0106aa3de1b956bc3ac
lfretz <levi.fretz@hsr.ch>
2020-06-30 17:00:42 +0200

    Ein try/catch Vorlage erstellt.

---
+++ A11_S269_Matruschka-Prinzip/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A11_S269_Matruschka_Prinzip
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Infos", ex);
+			}
+		}
+	}
+}
