72c1a97a879693b0947aba940c6b848b8e9510e4
lfretz7 <levi.fretz@outlook.com>
2020-06-30 09:25:52 +0200

    Weitere Exceptions in das Übungsscript eingebaut.

--- A11_S254_try_catch_finally/Program.cs
+++ A11_S254_try_catch_finally/Program.cs
@@ -8,13 +8,25 @@ namespace A11_S254_try_catch_finally
         {
             try
             {
-                string text = System.IO.File.ReadAllText("c:\\myFile.txt");
+                string text = System.IO.File.ReadAllText("C:\\Temp\\HTML.html");
                 Console.WriteLine("Der Text in der Datei lautet:");
                 Console.WriteLine(text);
             }
+            catch(System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Die Datei gibt's ja gar nicht!");
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                Console.WriteLine("Der Ordner zur Datei existiert nicht.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Der Zugriff auf die Datei wurde verweigert. Ätsch!");
+            }
             catch (Exception e)
             {
-                Console.WriteLine("Hoppala. Da gibt es ein Problem:");
+                Console.WriteLine("Hoppala. Da hat es etwas ganz Komisches:");
                 Console.WriteLine(e.Message);
             }
         }
