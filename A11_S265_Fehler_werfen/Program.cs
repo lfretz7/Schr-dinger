b36f9cc8397308b97f3ad01f33527d8e9d6f70cd
lfretz <levi.fretz@hsr.ch>
2020-06-30 16:25:42 +0200

    Selbst definierte Fehler gemacht.

---
+++ A11_S265_Fehler_werfen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A11_S265_Fehler_werfen
+{
+	public class SchroedingerException : Exception
+	{
+
+	}
+	public class SchroedingerArgumentException : ArgumentException
+	{
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			throw new Exception("Tja, so einfach kannst du einen Fehler auslösen!");
+
+		}
+	}
+}
