f54ba3a598561286ab4d96e19ec4efa3296f1b31
lfretz7 <levi.fretz@outlook.com>
2020-07-02 13:57:21 +0200

    ArrayList hinzugefügt.

---
+++ A12_S291_ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace A12_S291_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList list = new ArrayList();
+            list.Add(42);
+            list.Add(new object());
+            int theAnswerToAllQuestions = (int)list[0];
+
+
+            int[] numbers = new int[1000];
+            numbers[10] = 1;
+            int searchNumber = 100;
+            bool found = false;
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] == searchNumber)
+                {
+                    found = true;
+                    Console.WriteLine("Gefunden!");
+                    break;
+                }
+        }
+    }
+}
