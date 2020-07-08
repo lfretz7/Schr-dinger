a1c5ad554880ba881ed2281cad721557b53c3a4a
Levi Fretz <levi.fretz@ost.ch>
2020-07-08 15:17:22 +0200

    Den Unterschied zwischen einer sortierten Liste und einem Wörterbuch aufgezeigt.

---
+++ A12_S300_Difference_SortedList_Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A12_S300_Difference_SortedList_Dictionary
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> myDict = new Dictionary<int, string>();
+            if (!myDict.ContainsKey(4))
+            {
+                myDict.Add(4, "Schrödinger");
+            }
+
+            myDict.Add(1, "Freundin");
+            foreach (KeyValuePair<int, string> item in myDict)
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+
+        }
+    }
+}
